901d5be1af2c19d7c5b7cd30771e6af7ff6f516b
Jair Villalobos <juanjair2018@hotmail.com>
2023-08-26 04:23:19 -0500

    changes ui invoices

--- lib/invoices/screens/invoice_screen.dart
+++ lib/invoices/screens/invoice_screen.dart
@@ -68,104 +68,111 @@ class InvoiceListState extends State<InvoiceList> {
           title: const Text('Invoices'),
         ),
         body: ListView.builder(
-          itemCount: invoices.length + (page <= totalPages ? 1 : 0),
-          itemBuilder: (context, index) {
-            if (index == invoices.length) {
-              return _buildProgressIndicator();
-            } else {
-              final invoice = invoices[index];
-              return Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-                elevation: 4.0,
-                margin: const EdgeInsets.all(16.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Invoice ID: ${invoice['id'].toString()}',
-                        style: const TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 8),
-                      Text('Date: ${invoice['fecha']}'),
-                      const SizedBox(height: 16),
-                      const Text(
-                        'Company Information',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 8),
-                      Text('Name: ${invoice['empresa']['nombre']}'),
-                      Text('TIN: ${invoice['empresa']['nit']}'),
-                      const SizedBox(height: 16),
-                      const Text(
-                        'Operator Information',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 8),
-                      Row(children: [
-                        const Icon(Icons.person),
-                        const SizedBox(width: 8),
-                        Text(invoice['operador']['nombre']),
-                      ]),
-                      Text(
-                          'Document Type: ${invoice['operador']['tipo_documento']}'),
-                      Text('Document: ${invoice['operador']['documento']}'),
-                      Text(
-                          'Document Issuance City: ${invoice['operador']['ciudad_expedicion_documento']}'),
-                      Row(children: [
-                        const Icon(Icons.phone),
-                        const SizedBox(width: 8),
-                        Text(invoice['operador']['celular']),
-                      ]),
-                      Row(children: [
-                        const Icon(Icons.account_balance),
-                        const SizedBox(width: 8),
-                        Text(invoice['operador']['banco']),
-                      ]),
-                      Text(
-                          'Bank Account Number: ${invoice['operador']['numero_cuenta_bancaria']}'),
-                      Text(
-                          'Bank Account Type: ${invoice['operador']['tipo_cuenta_bancaria']}'),
-                      const SizedBox(height: 16),
-                      const Text(
-                        'Services',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 8),
-                      for (int i = 0; i < invoice['servicios'].length; i++)
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(children: [
-                                const Icon(Icons.description),
-                                const SizedBox(width: 8),
-                                Expanded(
-                                  child: Text(
-                                      invoice['servicios'][i]['descripcion']),
-                                ),
-                              ]),
-                              Row(children: [
-                                const Icon(Icons.attach_money),
-                                const SizedBox(width: 8),
-                                Text(invoice['servicios'][i]['valor']
-                                    .toString()),
-                              ])
-                            ]),
-                    ],
+  itemCount: invoices.length + (page <= totalPages ? 1 : 0),
+  itemBuilder: (context, index) {
+    if (index == invoices.length) {
+      return _buildProgressIndicator();
+    } else {
+      final invoice = invoices[index];
+      return Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        elevation: 4.0,
+        margin: const EdgeInsets.all(16.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Invoice ID: ${invoice['id'].toString()}',
+                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
+                  Text('Date: ${invoice['fecha']}'),
+                ],
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Company Information',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Text('Name: ${invoice['empresa']['nombre']}'),
+              Text('TIN: ${invoice['empresa']['nit']}'),
+              const SizedBox(height: 16),
+              const Text(
+                'Operator Information',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children:[
+                  const Icon(Icons.person),
+                  const SizedBox(width :8),
+                  Text(invoice['operador']['nombre']),
+                ]
+              ),
+              Text('Document Type: ${invoice['operador']['tipo_documento']}'),
+              Text('Document: ${invoice['operador']['documento']}'),
+              Text('Document Issuance City: ${invoice['operador']['ciudad_expedicion_documento']}'),
+              Row(
+                children:[
+                  const Icon(Icons.phone),
+                  const SizedBox(width :8),
+                  Text(invoice['operador']['celular']),
+                ]
+              ),
+              Row(
+                children:[
+                  const Icon(Icons.account_balance),
+                  const SizedBox(width :8),
+                  Text(invoice['operador']['banco']),
+                ]
+              ),
+              Text('Bank Account Number: ${invoice['operador']['numero_cuenta_bancaria']}'),
+              Text('Bank Account Type: ${invoice['operador']['tipo_cuenta_bancaria']}'),
+              const SizedBox(height :16),
+              const Text(
+                'Services',
+                style : TextStyle(fontSize :18, fontWeight :FontWeight.bold)
+              ),
+              const SizedBox(height :8),
+              for (int i = 0; i < invoice['servicios'].length; i++)
+                Column(
+                  crossAxisAlignment : CrossAxisAlignment.start,
+                  children:[
+                    Row(
+                      children:[
+                        const Icon(Icons.description),
+                        const SizedBox(width :8),
+                        Expanded(child:
+                          Text(invoice['servicios'][i]['descripcion']),
+                        ),
+                      ]
+                    ),
+                    Row(
+                      children:[
+                        const Icon(Icons.attach_money),
+                        const SizedBox(width :8),
+                        Text(invoice['servicios'][i]['valor'].toString()),
+                      ]
+                    )
+                  ]
                 ),
-              );
-            }
-          },
-          controller: _controller,
-        ));
+            ],
+          ),
+        ),
+      );
+    }
+  },
+  controller:_controller,
+)
+
+);
   }
 
   Widget _buildProgressIndicator() {
